db05013eb1c65d85d34088a41c249b34b2e419b3
Quang Khai Le <wzx173@haw-hamburg.de>
2025-06-02 01:30:36 +0200

    added google sign in to login page

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,18 +1,19 @@
 import { Component } from '@angular/core';
 import {RouterLink} from "@angular/router";
 import {FooterComponent} from "../../shared/footer/footer.component";
+import { GoogleSigninComponent } from "../../shared/google-sign-in/google-sign-in.component";
 
 @Component({
   selector: 'app-login',
   standalone: true,
   imports: [
     RouterLink,
-    FooterComponent
-  ],
+    FooterComponent,
+    GoogleSigninComponent
+],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
 
 }
- 
