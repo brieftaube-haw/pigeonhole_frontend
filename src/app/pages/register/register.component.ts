edd54f3c778aa1b0692eadadc2d622473bfd5ab0
e13as <elias.akbari@yahoo.de>
2025-06-03 17:22:19 +0200

    feature/feature/login-page| adjust register component

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,17 +1,40 @@
 import { Component } from '@angular/core';
-import {RouterLink} from "@angular/router";
-import {FooterComponent} from "../../shared/footer/footer.component";
+import { FormsModule } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+import { FooterComponent } from '../../shared/footer/footer.component';
 import { GoogleSigninComponent } from '../../shared/google-sign-in/google-sign-in.component';
+import { UserService, RegisterPayload } from '../../services/user.service';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [FooterComponent,
+  imports: [
+    FormsModule,       // <--- Wichtig für ngModel!
+    RouterLink,
+    FooterComponent,
     GoogleSigninComponent
   ],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrls: ['./register.component.css']
 })
 export class RegisterComponent {
+  benutzerName = '';
+  password = '';
+  passwordConfirm = '';
+  feedback = '';
 
+  constructor(private userService: UserService) {}
+
+  register() {
+    const payload: RegisterPayload = {
+      benutzerName: this.benutzerName,
+      password: this.password,
+      passwordConfirm: this.passwordConfirm
+    };
+
+    this.userService.registrieren(payload).subscribe({
+      next: () => this.feedback = 'Registrierung erfolgreich!',
+      error: err => this.feedback = 'Fehler: ' + err.error
+    });
+  }
 }
